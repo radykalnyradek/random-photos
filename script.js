9ac5c590b9f61ce1b806729dc8733b0972139790
Mateusz <matekumateku@gmail.com>
2023-04-15 21:21:26 +0200

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const imageContainer = document.querySelector(".image-container");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  for (i = 0; i < 3; i++) {
+    const img = document.createElement("img");
+    const random = Math.floor(Math.random() * 2000);
+    console.log(random);
+    img.src = "https://picsum.photos/300?random=" + random;
+    imageContainer.appendChild(img);
+  }
+});
